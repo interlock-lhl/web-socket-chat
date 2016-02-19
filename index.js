016311ff562f48f55931b9e8af7b27eb090193f7
James Sapara <james.sapara@gmail.com>
2016-02-19 10:03:55 -0800

    use env port if set

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var crypto = require('crypto');
+var port = process.env['PORT'] || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(3000, function() {
-  console.log('listen on port 3000');
+server.listen(port, function() {
+  console.log('listen on port ' + port);
 });
 
 io.on('connection', function(socket) {
