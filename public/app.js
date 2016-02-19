de822b3d9e4a8302845f7d3b0d04f564894cd94e
James Sapara <james.sapara@gmail.com>
2016-02-19 08:32:34 -0800

    template

--- public/app.js
+++ public/app.js
@@ -1,12 +1,3 @@
 $(function() {
-  var socket = io();
 
-  socket.on('message', function(data) {
-    console.log(data);
-  });
-
-  $('.ping').on('click', function() {
-    console.log('ping click');
-    socket.emit('message', 'meow');
-  });
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 
-- npm install socket.io express
+Uses express.js and socket.io to make a chat room. Style with bootstrap and made with love.
+
+https://en.gravatar.com/
+
+http://api.adorable.io/avatars/285/cat
