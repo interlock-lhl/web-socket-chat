26172768803e9ea872eb6a457746db26fb4ca60f
James Sapara <james.sapara@gmail.com>
2016-06-07 08:32:22 -0700

    various small updates for demo

--- public/app.js
+++ public/app.js
@@ -1,11 +1,9 @@
 $(function() {
   var socket = io();
   var chatTemplate = Handlebars.compile($('#chat-template').html());
-  var CHAT_LIMIT = 10;
 
   $('#message-send').on('click', function() {
     messageSend($('#message'));
-
   });
 
   $('#message').on('keyup', function(event) {
@@ -15,22 +13,14 @@ $(function() {
   });
 
   function messageSend(message) {
-    socket.emit('message-send', {message: message.val()});
-    message.val('');
+    insertChatMessage({message: message.val()});
   }
 
   function insertChatMessage(data) {
-    $('#messages').append(chatTemplate(data));
-    $('#messages').animate({scrollTop: $('#messages').height()});
-    var messages = $('#messages > div');
-    if (messages.length > CHAT_LIMIT) {
-      messages.slice(0, messages.length - CHAT_LIMIT).each(function(i, elm) {
-        elm.remove();
-      });
-    }
-  };
+    var messages = $('#messages');
+    messages.append(chatTemplate({id: "meow", message: data.message}));
+    messages.animate({ scrollTop: messages[0].scrollHeight}, 250);
+  }
 
-  socket.on('chat-event', function(data) {
-    insertChatMessage(data);
-  })
+
 });
